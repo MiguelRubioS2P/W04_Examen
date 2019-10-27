4a6e30c594f41cc86069601dc0d90fde8fa3a2ab
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2019-10-27 21:58:34 +0100

    guardar estado

---
+++ W05_ExamenRegistroEncontrar/Clases/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W05_ExamenRegistroEncontrar.Clases
+{
+    public class Usuario
+    {
+
+        public int ID_USR { get; set; }
+        public string DNI_USR { get; set; }
+        public string NOM_USR { get; set; }
+        public string LLINATGE1 { get; set; }
+        public string POB_USR { get; set; }
+        public string EMAIL_USR { get; set; }
+        public string PASSWORD { get; set; }
+        public string infoUser
+        {
+            get
+            {
+                return ID_USR + ":\t" + NOM_USR;
+            }
+        }
+
+    }
+}
--- W05_ExamenRegistroEncontrar/Main.cs
+++ W05_ExamenRegistroEncontrar/Main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace W05_ExamenRegistroEncontrar
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
